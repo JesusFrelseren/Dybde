2a86752b04df9b7ff552538064918fb4e05e3024
Tobias Pedersen <tobiasped@hotmail.com>
2017-10-28 11:41:42 +0200

    uppdate

--- src/oversikt.php
+++ src/oversikt.php
@@ -13,6 +13,7 @@ include("include/xml_reader.php");
     <table id="data">
         <tbody>
             <tr><th>Middelvann</th><th>Normalnull</th><th>Sjøkartnull</th></tr>
+            <tr><th>139 cm</th><th>120 cm</th><th>100 cm</th></tr>
            <?php var_dump(hentData("63.9", "9.4", "")); ?>
 
         </tbody>
