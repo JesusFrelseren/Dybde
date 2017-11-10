b1d2266d148ab87d63994733ef9ce26a3c1be4a1
JesusFrelseren <erlendahall@gmail.com>
2017-11-10 11:40:45 +0100

    fsf

--- src/reader.php
+++ src/reader.php
@@ -130,6 +130,8 @@ function lagreSammensattXML($res) {
         $url2 = "sammensatt.xml";
         $sxml2 = simplexml_load_file($url2);
         if(isset($sxml2->tide[1]->locationdata->data->waterlevel )){
+            $date = date_create($data2["time"]);
+            $dateFormatted = date_format($date, 'H:i');
             if($sxml2->tide[1]->locationdata->data["type"]=="observation"){
 
             try {
